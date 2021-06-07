c43478b355a04ff524a6b074fecfbaf2c63845c6
lujjjh <lujjjh@gmail.com>
2021-06-07 18:49:44 +0800

    Initialize smart HTTP protocol

--- cmd/gitig/main.go
+++ cmd/gitig/main.go
@@ -1,4 +1,14 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/lujjjh/gitig/git"
+)
+
 func main() {
+	if err := http.ListenAndServe(":3000", new(git.SmartHTTPHandler)); err != nil {
+		log.Fatal(err)
+	}
 }
